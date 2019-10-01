bb971e95397fb5dd64de304a8c93b34551a0dee8
xunzhanggzl <1243286264@qq.com>
2019-10-01 09:55:04 +0800

    Web App的功能

---
+++ demos/19.js
@@ -0,0 +1,14 @@
+// demos/19.js
+const Koa = require('koa');
+const app = new Koa();
+
+const main = function (ctx) {
+  const n = Number(ctx.cookies.get('view') || 0) + 1;
+  ctx.cookies.set('view', n);
+  ctx.response.body = n + ' views';
+}
+
+app.use(main);
+app.listen(3000, () => {
+  console.log(`http://localhost:3000/`);
+});
---
+++ demos/20.js
@@ -0,0 +1,18 @@
+// demos/20.js
+const Koa = require('koa');
+const koaBody = require('koa-body');
+const app = new Koa();
+
+const main = async function (ctx) {
+  const body = ctx.request.body;
+  if (!body.name) ctx.throw(400, '.name required');
+  ctx.body = {
+    name: body.name
+  };
+};
+
+app.use(koaBody());
+app.use(main);
+app.listen(3000, () => {
+  console.log(`http://localhost:3000/`);
+});
